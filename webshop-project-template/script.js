1e2cea5d91ed330ff750676c09e46f9f3cd84c97
rhzsolt@gmail.com <you@example.com>
2021-06-07 15:29:43 +0200

    8.16 renderProducts

--- webshop-project-template/script.js
+++ webshop-project-template/script.js
@@ -1,5 +1,11 @@
+/*C reate
+  R ead
+  U pdate
+  D elete*/
+
+
 var state = {
-produtcs :[
+products :[
 {
     name:'Teszt termék 1',
     price: 2500,
@@ -8,15 +14,43 @@ produtcs :[
 
 {
     name:'Teszt termék 2',
-    price: 3500,
+    price: 4500,
     isInStock: true, 
 },
 {
     name:'Teszt termék 3',
-    price: 5500,
-    isInStock: true, 
+    price: 6500,
+    isInStock: false, 
 }
 ]
-
-
 }
+/*
+var productsList = '';
+ 
+for( var prod of state.products){
+    productsList = productsList +`<div class =" card p-5 m-2 ">
+    ${prod.name}<br>
+    ${prod.price}
+    </div>
+    
+    `;
+}
+
+document.getElementById('product-list-component').innerHTML = productsList;
+EZ SAJÁT UA MINT AZ ALSÓ DE AZ FGV BE VAN SZERKESZTVE*/
+
+function renderProducts(){
+
+var productsHTML = '';
+for( var product of state.products){
+    productsHTML +=`<div class =" card p-5 m-2 ${product.isInStock ? '' : 'bg-danger'}">
+    <p>${product.name}</p><br>
+    <p>${product.price}</p>
+    </div>
+    `;}
+
+
+document.getElementById('product-list-component').innerHTML = productsHTML;
+}
+
+window.onload = renderProducts;
