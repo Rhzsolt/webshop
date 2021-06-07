48b4c231307aa52503fcdec788cf6a3eeac70f91
rhzsolt@gmail.com <you@example.com>
2021-06-07 13:10:03 +0200

    0

---
+++ webshop-project-template/script.js
@@ -0,0 +1,22 @@
+var state = {
+produtcs :[
+{
+    name:'Teszt termék 1',
+    price: 2500,
+    isInStock: true, 
+},
+
+{
+    name:'Teszt termék 2',
+    price: 3500,
+    isInStock: true, 
+},
+{
+    name:'Teszt termék 3',
+    price: 5500,
+    isInStock: true, 
+}
+]
+
+
+}
