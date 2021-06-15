058f31be42c76ef11ba4c0a58dfba936fabcb790
rhzsolt@gmail.com <you@example.com>
2021-06-15 17:49:27 +0200

    teljes és jó

--- webshop-project-template/script.js
+++ webshop-project-template/script.js
@@ -5,68 +5,98 @@
 
 
 var state = {
-products :[
-{
-    name:'Teszt termék 1',
-    price: 2500,
-    isInStock: true, 
-},
+    products: [
+        {
+            id: uuidv4(),
+            name: 'Teszt termék 1',
+            price: 2500,
+            isInStock: true,
+        },
 
-{
-    name:'Teszt termék 2',
-    price: 4500,
-    isInStock: true, 
-},
-{
-    name:'Teszt termék 3',
-    price: 6500,
-    isInStock: false, 
-}
-]
-}
-/*
-var productsList = '';
- 
-for( var prod of state.products){
-    productsList = productsList +`<div class =" card p-5 m-2 ">
-    ${prod.name}<br>
-    ${prod.price}
-    </div>
-    
-    `;
+        {
+            id: uuidv4(),
+            name: 'Teszt termék 2',
+            price: 4500,
+            isInStock: true,
+        },
+        {
+            id: uuidv4(),
+            name: 'Teszt termék 3',
+            price: 6500,
+            isInStock: false,
+        }
+    ]
 }
 
-document.getElementById('product-list-component').innerHTML = productsList;
-EZ SAJÁT UA MINT AZ ALSÓ DE AZ FGV BE VAN SZERKESZTVE*/
 
-function renderProducts(){
 
-var productsHTML = '';
-for( var product of state.products){
-    productsHTML +=`<div class =" card p-5 m-2 ${product.isInStock ? '' : 'bg-danger'}">
-    <p>${product.name}</p><br>
-    <p>${product.price}</p>
-    </div>
-    `;}
+function renderFunction() {
+    productsHTML = '';
 
-document.getElementById('product-list-component').innerHTML = productsHTML;
+    for (prod of state.products) {
+
+        productsHTML += `<div class = "card p-5 m-2">
+    ${prod.name},
+    ${prod.price},
+    <button class ="btn btn-danger delete-products" data-productsid="${prod.id}">Törlés</button>
+
+    </div>`
+    }
+
+    document.getElementById("product-list-component").innerHTML = productsHTML
+
+    for (var deleteBtn of document.querySelectorAll('.delete-products')) {
+
+        deleteBtn.onclick = function (event) {
+
+            var id = event.target.dataset.productsid;
+            var foundIndex;
+
+            for (var i = 0; i < state.products.length; i++) {
+
+                if (state.products[i].id === id) { foundIndex = i; break; }
+            }
+
+            state.products.splice(foundIndex, 1);
+
+            renderFunction();
+        }
+
+    }
+
 }
-window.onload = renderProducts;
 
-//ACTION
-document.getElementById('create-product').onsubmit = function(event){
-    event.preventDefault()
-    var price =Number((event.target.elements.price.value));
-    var name =(event.target.elements.name.value);
-    var stock =(event.target.elements.isInStock.checked);
+document.getElementById("create-product").onsubmit = function (event) {
 
-//STATE CHANGE
-state.products.push({
-    name : name,
-    price: price,
-    isInStock:stock,
-});
-//RENDER
-    renderProducts();
+    event.preventDefault();
+    var name = event.target.elements.name.value;
+    var pice = event.target.elements.price.value;
+    var stock = event.target.elements.isInStock.checked;
 
+
+    state.products.push({
+        id: uuidv4(),
+        name: name,
+        price: pice,
+        isInStock: stock,
+    }
+    )
+    renderFunction()
+};
+
+
+
+
+
+window.onload = renderFunction;
+
+
+
+
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
 }
