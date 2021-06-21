a361655c54a90279f6d0e26efa2855e9a9d30034
Programozas Karrier <info@programozaskarrier.hu>
2021-06-21 21:09:21 +0200

    update eleje

--- webshop-project-template/script.js
+++ webshop-project-template/script.js
@@ -5,84 +5,134 @@
 
 
 var state = {
-  product: [
-
+  products: [
     {
       id: uuidv4(),
-      name: 'Bogi',
-      age: 18,
-      atHome: true,
+      name: 'alma',
+      price: 150,
+      isInBox: true,
     },
+
     {
       id: uuidv4(),
-      name: 'Zsolt',
-      age: 53,
-      atHome: true,
+      name: 'körte',
+      price: 250,
+      isInBox: true,
     },
+
     {
       id: uuidv4(),
-      name: 'Marta',
-      age: 53,
-      atHome: true,
-    }
+      name: 'nari',
+      price: 350,
+      isInBox: true,
+    },
+  ],
+  editedId: ''
 
-
-  ]
 }
 
-function Family() {
-
-  var gyujt = '';
-
-  for (fam of state.product) {
-
-    gyujt += `<div class ="card p-5 ${fam.atHome ? '' : 'bg-warning'}">
-    
-    ${fam.name}
-    ${fam.age}
-    <button class=' btn btn-danger törles' data-ides='${fam.id}'>TÖRÖLŐ</button>
-    </div>`};
-document.getElementById("product-list-component").innerHTML = gyujt;
-
-  for (delbtn of document.querySelectorAll('.törles')) {
-    
-
-    delbtn.onclick = function (event) {
-      var idek = event.target.dataset.ides;
-      var tal;
-
-      for (var i = 0; i < state.product.length; i++) {
-        if (state.product[i].id === idek) { tal = i; break; }
+function renderEditProduct() {
+  var foundProduct;
+  
+  for (var product of state.products) {
+    if (product.id === state.editedId) {
+      foundProduct = product;
+      break;
       }
-      state.product.splice(tal, 1);
 
-      Family();
+  }
+  var editFormHTML = `
+  <h3>Termék szerkesztése</h3>
+    <form id="update-product" class="p-5">
+            <label class="w-100">
+              Név:
+              <input class="form-control" type="text" name="name" value ="${foundProduct.name}">
+            </label>
+            <label class="w-100">
+              Kor:
+              <input class="form-control" type="number" name="price" value ="${foundProduct.price}">
+            </label>
+            <label class="w-100">
+             Otthon van?
+              <input class="form-control" type="checkbox" name="isInBox"${foundProduct.isInBox ? 'checked': ''}> 
+            </label>
+            <button class="btn btn-primary" type="submit">Küldés</button>
+        </form>
+  `
+  document.getElementById("edit-product").innerHTML = editFormHTML;
+}
+
+function renderState() {
+
+  productHTML = '';
+
+  for (prod of state.products) {
+
+    productHTML +=
+      `
+    <div class =' card p-5 floatright delete ${prod.isInBox ? '' : 'bg-warning'}'>
+
+      ${prod.name},
+      ${prod.price},
+      <button class='bg-warning edit-product mb-2' data-productid='${prod.id}'>SZERKESZTŐ</button>
+      <button class='bg-danger törölő' data-productid='${prod.id}'>TÖRÖLŐ</button>
+    </div>`
+
+
+  }
+
+  document.getElementById("product-list-component").innerHTML = productHTML;
+
+
+
+  for (var editBtn of document.querySelectorAll('.edit-product')) {
+
+    editBtn.onclick = function (event) {
+      var id = event.target.dataset.productid;
+      /* state változás után*/
+      state.editedId = id;
+      renderEditProduct();
 
     }
   }
 
-  
+  for (dleteBtn of document.querySelectorAll('.törölő')) {
+    dleteBtn.onclick = function (event) {
+      var delet;
+      var trl = event.target.dataset.productid
+
+      for (var i = 0; i < state.products.length; i++) {
+
+        if (state.products[i].id === trl) { trl = delet; break; }
+      }
+      state.products.splice(delet, 1);
+      renderState();
+
+    }
+
+  }
+
+
+
+
 }
-document.getElementById("create-product").onsubmit = function(event){
-  event.preventDefault()
 
-var name = event.target.elements.name.value;
-var age =Number(event.target.elements.age.value);
-var otth = event .target.elements.atHome.checked;
+document.getElementById("create-product").onsubmit = function (event) {
 
-state.product.push({
-  id:uuidv4(),
-  name:name,
-  age:age,
-  atHome:otth,
-})
-Family();
+  event.preventDefault();
+
+  var name = event.target.elements.name.value;
+  var price = Number(event.target.elements.price.value);
+  var isIn = event.target.elements.isInBox.checked;
+
+  state.products.push({
+    id: uuidv4(),
+    name: name,
+    price: price,
+    isInBox: isIn,
+  })
+  renderState();
 }
-
-
-
-
-
 
 
 function uuidv4() {
@@ -91,4 +141,4 @@ function uuidv4() {
     return v.toString(16);
   });
 }
-window.onload = Family();
+window.onload = renderState();
