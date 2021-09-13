6116bfa2a9c25325c4e2d2635edb5e7713b601f2
Programozas Karrier <info@programozaskarrier.hu>
2021-09-13 20:46:05 +0200

    delete

--- webshop-project-template/script.js
+++ webshop-project-template/script.js
@@ -14,19 +14,20 @@ Delete*/
 /*read*/
 var state ={
   products:[{
+    id:uuidv4(),
       name:'Trappista',
       price:1500,
       isInStock:false,
   
   },
 
-  {
+  {id:uuidv4(),
       name:'Edami',
       price:2500,
       isInStock:false,
   },
 
-  {
+  {id:uuidv4(),
       name:'Gouda',
       price:3500,
       isInStock:true,
@@ -39,17 +40,38 @@ function renderProducts() {
 for ( var product of state.products){
 
   productHTML +=`
-  <div class ="card m-2 p-2 ${product.isInStock ? '' : 'bg-danger'}">
+  <div class ="card m-2 p-2 ${product.isInStock ? '' : 'bg-primary'}">
  <p>${product.name}</p>
  <p>${product.price}</p> 
+ <button class = "btn btn-danger float right button-trl" data-trl ="${product.id}">Törlő</button>
  </div>
   `;
 }
 
 document.getElementById("product-list-component").innerHTML = productHTML;
+ 
+for( var tbtn of document.querySelectorAll('.button-trl')){
 
+  tbtn.onclick =function(event){
+
+    var btn = event.target.dataset.trl;
+    
+
+ var joid;
+  for(var i=0; i<state.products.length;i++){
+
+    
+    if( state.products[i].id === btn ){ joid = i;break;}
+  }
+
+state.products.splice(joid,1);
+renderProducts();
+  }
+  
+ 
 }
-window.onload = renderProducts();
+}
+
 
 document.getElementById("create-product").onsubmit = function (event) {
   event.preventDefault();
@@ -59,6 +81,7 @@ document.getElementById("create-product").onsubmit = function (event) {
   var isInStock = event.target.elements.isInStock.checked;
 
 state.products.push({
+  id:uuidv4(),
   name:name,
   price:price,
   isInStock:isInStock});
@@ -66,4 +89,11 @@ state.products.push({
   renderProducts();
 
 }
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
  
+window.onload = renderProducts();
